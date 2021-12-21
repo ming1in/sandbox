6468e38709f6579cf3d8864d1ddacc5e839e6cbb
Ming Lin <minglianglin50@gmail.com>
2021-12-21 17:55:02 -0500

    next fun

---
+++ javascript/GlobalMaximum.py
@@ -0,0 +1,16 @@
+import itertools
+
+def getCurrMin(x, y):
+  tempMax = 0
+  absval = abs(x-y)
+  if tempMax < absval:
+    
+  
+
+def findMaximum(arr, n):
+    perms = set(itertools.permutations(arr, n))
+    return perms
+      
+
+
+print(findMaximum([2, 3, 5], 2))
